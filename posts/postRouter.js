3a81d2385d677dce5747c496bc6950c3f35bebda
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-12-02 18:16:00 -0500

    PostRouter is done too

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Posts = require('./postDb');
 
 const router = express.Router();
 
@@ -22,6 +23,16 @@ router.put('/:id', (req, res) => {
 
 function validatePostId(req, res, next) {
   // do your magic!
+  const id = req.params.id;
+  Posts.getById(id)
+    .then((post) => {
+      req.post = post;
+      next();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({ error: 'the specified post ID does not exist.' });
+    });
 }
 
 module.exports = router;
